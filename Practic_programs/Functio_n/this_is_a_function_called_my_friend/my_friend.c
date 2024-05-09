66cdb1df1cb2066446dc9978529ca786e3328c08
farhan99990 <sadikfarhan038@gmail.com>
2024-05-09 22:23:08 +0600

    pera nai chill

---
+++ Practic_programs/Functio_n/this_is_a_function_called_my_friend/my_friend.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int myfriend()
+{
+    printf("pera nai!! chilll!!\n");
+    return 6;
+}
+int main()
+{
+    int a;
+    a = myfriend();
+    printf("%d\n",a);
+    myfriend();
+    return 0;
+}
